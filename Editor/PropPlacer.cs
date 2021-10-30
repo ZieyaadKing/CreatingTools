bf7b702a46ef12840cfbd90cc98029d5e3a1740c
Zieyaad King <k.m.zieyaaad@gmail.com>
2021-10-30 13:21:40 +0200

    Added Tangent Space

--- Editor/PropPlacer.cs
+++ Editor/PropPlacer.cs
@@ -12,6 +12,8 @@ public class PropPlacer : EditorWindow
     #region Properties
     public float radius   = 2f;
     public int spawnCount = 8;
+
+    Vector2[] randomPoints;
     #endregion
 
     #region Serialized Properties
@@ -25,6 +27,8 @@ public class PropPlacer : EditorWindow
         so             = new SerializedObject(this);
         propRadius     = so.FindProperty("radius");
         propSpawnCount = so.FindProperty("spawnCount");
+
+        GenerateRandomPoints();
 
         SceneView.duringSceneGui += DuringSceneGUI;
     }
@@ -41,12 +45,58 @@ public class PropPlacer : EditorWindow
         so.Update();
         EditorGUILayout.PropertyField(propRadius);
         EditorGUILayout.PropertyField(propSpawnCount);
-        so.ApplyModifiedProperties();
+
+        // Any changes made in the editor will immediately be updated
+        // in the sceneview
+        if (so.ApplyModifiedProperties()) SceneView.RepaintAll();
     }
 
     // Handles everything in the scene view (Props, diagrams, etc)
     void DuringSceneGUI(SceneView sceneView) {
+        if (Event.current.type != EventType.Repaint) return;
 
+        Transform cameraTransform = sceneView.camera.transform;
+
+        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
+
+            // Setting up the tangent space
+            // Tangent space: A space without any origin
+            Vector3 hitNormal     = hit.normal;
+            Vector3 hitTangent    = Vector3.Cross(hitNormal, cameraTransform.up);
+            Vector3 hitBitangent  = Vector3.Cross(hitNormal, hitTangent);
+
+            foreach (Vector2 point in randomPoints)
+            {
+                Vector3 worldPos = hit.point + (hitTangent * point.x + hitBitangent * point.y) * radius;
+                DrawSphere(worldPos);
+            }
+
+            // Drawing tanngent space
+            Handles.color = Color.blue;
+            Handles.DrawAAPolyLine(5, hit.point, hit.point + hitTangent);
+            Handles.color = Color.green;
+            Handles.DrawAAPolyLine(5, hit.point, hit.point + hitBitangent);
+            Handles.color = Color.red;
+            Handles.DrawAAPolyLine(5, hit.point, hit.point + hitNormal);
+            Handles.color = Color.white;
+
+            Handles.DrawWireDisc(hit.point, hit.normal, radius);
+            
+        }
     } 
     #endregion
+
+    #region Draw Methods
+    void DrawSphere(Vector3 pos) {
+        Handles.SphereHandleCap(-1, pos, Quaternion.identity, .1f, EventType.Repaint);
+    }
+    #endregion
+    void GenerateRandomPoints() {
+        randomPoints = new Vector2[spawnCount];
+        for (int i = 0; i < randomPoints.Length; i++)
+        {
+            randomPoints[i] = Random.insideUnitCircle;
+        }
+    }
 }
